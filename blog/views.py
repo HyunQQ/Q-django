4c08ca511559814aec6353a9d3786d7269b63b85
hyunq <5hyung@datasolution.kr>
2019-04-23 10:20:03 +0900

    blog template 추가 및 css 적용

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -11,3 +11,12 @@ from .models import Post
 def post_list(request):
     posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html',{'posts':posts})
+
+def post_detail(request, pk):
+    # try:
+    #     post = Post.objects.get(pk=pk) # Post.DoesNotExist pk 가 없을 경우 500에러 처리를 막아준다.
+    # except Post.DoesNotExist:
+    #     raise Http404   # django.http.Http404
+
+    post = get_object_or_404(Post, pk=pk) # 위의 4줄과 같은 역할
+    return render(request, 'blog/post_detail.html', {'post':post})
