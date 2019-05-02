7274b660fa191d16635f66443c46c0521f02d33c
hyunq <5hyung@datasolution.kr>
2019-05-02 14:33:14 +0900

    add STATIC_URL

--- dev_web/settings.py
+++ dev_web/settings.py
@@ -133,6 +133,7 @@ USE_TZ = True
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
